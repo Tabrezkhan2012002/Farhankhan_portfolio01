4fc5f4c5184c021b6b53dc4f5ec0ec5b3e45494b
Tabrezkhan2012002 <tabrezkhan950740@gmail.com>
2026-03-13 23:26:40 +0530

    set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/farhankhan/",  // Important: your repo name
+  base: "/farhankhan/", // This must match your GitHub repo name
 })
