f346f8181af0d149542979dc7b5c805e1563360a
Tabrezkhan2012002 <tabrezkhan950740@gmail.com>
2026-03-14 17:19:43 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:"/mfk/",
+  base: '/Farhankhan_portfolio01/',
   plugins: [react()],
 })
